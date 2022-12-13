b7a5e9ed58b7852596b63fe8229b65d5d0e42a6d
220114228 <220114228@tut4life.ac.za>
2022-12-13 18:13:33 +0200

    Entry Activity updated

--- app/src/main/java/com/example/stockmanager/AboutActivity.java
+++ app/src/main/java/com/example/stockmanager/AboutActivity.java
@@ -6,8 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.PopupMenu;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -15,7 +17,31 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
         ImageView about_menu = findViewById(R.id.about_menu);
+        TextView purchase_entry_title = findViewById(R.id.purchase_entry_title);
+        TextView item_name = findViewById(R.id.item_name);
+        EditText enterItem = findViewById(R.id.enterItem);
+        TextView category = findViewById(R.id.category);
+        EditText enterCategory = findViewById(R.id.enterCategory);
+        ImageView dropdown_menu1 = findViewById(R.id.dropdown_menu1);
+        ImageView add_category = findViewById(R.id.add_category);
+        TextView price_bought = findViewById(R.id.price_bought);
+        EditText priceBought = findViewById(R.id.priceBought);
+        TextView quantity = findViewById(R.id.quantity);
+        EditText enterQuantity = findViewById(R.id.enterQuantity);
+        ImageView dropdown_menu2 = findViewById(R.id.dropdown_menu2);
+        TextView priceSold = findViewById(R.id.priceSold);
+        EditText selling_Price = findViewById(R.id.selling_Price);
+        TextView dateEntry = findViewById(R.id.dateEntry);
+        EditText enterDate = findViewById(R.id.enterDate);
+        ImageView dropdown_menu3 = findViewById(R.id.dropdown_menu3);
+        TextView barCode = findViewById(R.id.barCode);
+        EditText scanBarCode = findViewById(R.id.scanBarCode);
+
+
+
+
     }
     private void showMenu(View view){
         PopupMenu popupMenu = new PopupMenu(AboutActivity.this,view);
--- app/src/main/java/com/example/stockmanager/EntryActivity.java
+++ app/src/main/java/com/example/stockmanager/EntryActivity.java
@@ -6,8 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.PopupMenu;
+import android.widget.TextView;
 
 public class EntryActivity extends AppCompatActivity {
 
@@ -15,6 +18,27 @@ public class EntryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry);
+        TextView purchase_entry_title = findViewById(R.id.purchase_entry_title);
+        TextView item_name = findViewById(R.id.item_name);
+        EditText enterItem = findViewById(R.id.enterItem);
+        TextView category = findViewById(R.id.category);
+        EditText enterCategory = findViewById(R.id.enterCategory);
+        EditText priceBought = findViewById(R.id.priceBought);
+        TextView price_bought= findViewById(R.id.price_bought);
+        TextView quantity = findViewById(R.id.quantity);
+        EditText enterQuantity = findViewById(R.id.enterQuantity);
+        TextView priceSold = findViewById(R.id.priceSold);
+        EditText selling_Price = findViewById(R.id.selling_Price);
+        TextView dateEntry = findViewById(R.id.dateEntry);
+        EditText enterDate = findViewById(R.id.enterDate);
+        TextView barCode = findViewById(R.id.barCode);
+        EditText scanBarCode = findViewById(R.id.scanBarCode);
+        Button add_button = findViewById(R.id.add_button);
+        ImageView dropdown_menu1 = findViewById(R.id.dropdown_menu1);
+        ImageView add_category = findViewById(R.id.add_category);
+        ImageView dropdown_menu2 = findViewById(R.id.dropdown_menu2);
+        ImageView dropdown_menu3 = findViewById(R.id.dropdown_menu3);
+        ImageView camera = findViewById(R.id.camera);
         ImageView menu_bar =findViewById(R.id.entryactivity_menu);
         menu_bar.setOnClickListener(new View.OnClickListener() {
             @Override
